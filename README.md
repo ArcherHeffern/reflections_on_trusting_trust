c9e0bafc72f016addbd91198a96284d2fe8ea1d4
archer <archerheffern@gmail.com>
2025-01-18 13:55:28 +0000

    reorg

---
+++ README.md
@@ -0,0 +1,8 @@
+Based on Ken Thompsons 1984 AM Turing Award Speech, "Reflections on Trusting Trust"  
+
+This project is split into 3 steps:
+1. Directly modifying login.c source code with a backdoor
+2. Modifying the compiler to inject a backdoor into login.c during comptime
+3. Modifying the compiler to inject a backdoor into itself during comptime, to inject the backdoor into login.c. This makes the backdoor undetectable, even with login.c and the compiler source code. 
+
+Thank you @Rui314 for the C compiler source code. All I did was make the backdoor modifications. 
---
+++ login.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+// Getc, gets, fgets, scanf, sscanf, getline
+
+#define MAX_USERS 8
+#define MAX_STRLEN 64
+
+int num_users = 0;
+char* usernames[MAX_USERS];
+char* passwords[MAX_USERS];
+
+void create_account(char* username, char* password) {
+	if (num_users == MAX_USERS) {
+		fprintf(stderr, "Max users created\n");
+		return;
+	}
+	for (int i = 0; i < num_users; i++) {
+		if (strcasecmp(usernames[num_users], username) == 0) {
+			fprintf(stderr, "User already exists\n");	
+			return;
+		}
+	}
+	usernames[num_users] = username;
+	passwords[num_users] = password;
+	num_users++;
+}
+
+void login(char* username, char* password) {
+	char* user_index = -1;
+	for (int i = 0; i < num_users; i++) {
+		if (strcasecmp(usernames[i], username) == 0) {
+			user_index = i;
+			break;
+		}
+	}
+	if (user_index == -1) {
+		fprintf(stderr, "Username not found\n");
+		return;
+	}
+	if (strcmp(passwords[user_index], password) == 0) {
+		printf("logged in!\n");
+	} else {
+		fprintf(stderr, "Bad password\n");
+	}
+}
+
+void list_users() {
+	for (int i = 0; i < num_users; i++) {
+		printf("%s ", usernames[i]);
+	}
+	printf("\n");
+}
+
+void prompt(char* msg, char* buf) {
+	printf("%s", msg);
+	int n_read = fgets(buf, MAX_STRLEN, stdin);
+	if (n_read != 0 && buf[n_read-1] == '\n') {
+		buf[n_read-1] = 0;
+	}
+}
+
+int main() {
+	while (1) {
+		char op[MAX_STRLEN];
+		char username[MAX_STRLEN];
+		char password[MAX_STRLEN];
+		prompt("Operation? (Create, Login, List, Quit): ", op);
+		if (strcasecmp(op, "create") == 0) {
+			prompt("Username: ", username);
+			prompt("Password: ", password);
+			create_account(username, password);
+		} else if (strcasecmp(op, "login") == 0) {
+			prompt("Username: ", username);
+			prompt("Password: ", password);
+			login(username, password);
+		} else if (strcasecmp(op, "list") == 0) {
+			list_users();
+		} else if (strcasecmp(op, "exit") == 0) {
+			break;
+		} else {
+			fprintf(stderr, "Unrecognized command\n");
+	}
+	printf("Exiting...\n");
+}

